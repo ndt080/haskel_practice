308edbc2259b3e8b32bfe645bc8a9598e242d13f
Andrei Petrov <68751397+ndt080@users.noreply.github.com>
2022-11-08 20:03:15 +0000

    implement gcd

---
+++ Main.hs
@@ -0,0 +1,12 @@
+-- WORKFILE: to run in replit
+
+eGcd :: Int -> Int -> Int
+eGcd a b
+  | a <= 0 = 0
+  | b <= 0 = 0
+  | a > b = eGcd b (a - b)
+  | a < b = eGcd (b - a) a
+  | otherwise = a
+
+main = do
+  print (eGcd 3 27)
