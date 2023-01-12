69911d2403af987dd5d98066d9f425d99a0e2388
andrei petrov <68751397+ndt080@users.noreply.github.com>
2023-01-12 14:00:16 +0000

    collatz, delannoy

--- Lesson04.hs
+++ Lesson04.hs
@@ -27,9 +27,30 @@ tailPowFn x p acc
 tailPow :: (Integral x, Num p) => p -> x -> p
 tailPow x p = tailPowFn x p 1
 
--- 04: PERFECT
+-- 04: Perfect numbers
 perfect :: Integral a => a -> Bool
 perfect n = n == sum [i | i <- [1..n-1], n `mod` i == 0]
+
+-- 05: The Syracuse Sequence 
+collatzFn :: Int -> Int -> Int
+collatzFn x acc
+  | x == 1 = acc
+  | even x = collatzFn (div x 2) (acc + 1)
+  | otherwise = collatzFn (3 * x + 1) (acc + 1)
+
+collatz :: Int -> Int
+collatz x = collatzFn x 1
+
+-- 06: Delannois numbers
+delannoy :: Int -> Int -> Int
+delannoy n m 
+  | n == 0 = 1
+  | m == 0 = 1
+  | otherwise = 
+    let
+      n' = n - 1
+      m' = m - 1
+    in delannoy n' m + delannoy n' m' + delannoy n m'
 
 -- TESTING MODULE
 testLesson04 :: IO ()
@@ -39,4 +60,6 @@ testLesson04 = do
   print ("02: pow      (3, 4):     ", pow 3 4)
   print ("02: tailPow  (3, 4):     ", tailPow 3 4)
   print ("04: perfect    (28):     ", perfect 28)
+  print ("05: collatz     (7):     ", collatz 7)
+  print ("06: delannoy (3, 4):     ", delannoy 3 4)
   print ("-------------------------------")
