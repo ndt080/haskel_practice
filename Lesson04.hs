af1a5758c9374a3c5c3ff64a63c9b36678164524
andrei petrov <68751397+ndt080@users.noreply.github.com>
2023-01-12 13:04:45 +0000

    perfect fn

--- Lesson04.hs
+++ Lesson04.hs
@@ -1,6 +1,6 @@
 module Lesson04 (eGcd, pow, testLesson04) where
 
--- GCD
+-- 01: GCD
 eGcd :: Int -> Int -> Int
 eGcd a b
   | a <= 0 = 0
@@ -9,7 +9,7 @@ eGcd a b
   | a < b = eGcd (b - a) a
   | otherwise = a
 
--- POW
+-- 02: POW
 pow :: (Integral x, Num p) => p -> x -> p
 pow x p
   | p == 0 = 1
@@ -17,7 +17,7 @@ pow x p
   | even p = pow (x * x) (div p 2)
   | otherwise = x * pow x (p - 1)
 
--- POW with tail recursion
+-- 02: POW with tail recursion
 tailPowFn :: (Integral x, Num p) => p -> x -> p -> p
 tailPowFn x p acc
   | p == 0 = acc
@@ -27,12 +27,16 @@ tailPowFn x p acc
 tailPow :: (Integral x, Num p) => p -> x -> p
 tailPow x p = tailPowFn x p 1
 
+-- 04: PERFECT
+perfect :: Integral a => a -> Bool
+perfect n = n == sum [i | i <- [1..n-1], n `mod` i == 0]
 
 -- TESTING MODULE
 testLesson04 :: IO ()
 testLesson04 = do
-  print ("------: LESSON 04 :------")
-  print ("eGcd(3, 27):    ", eGcd 3 27)
-  print ("pow(3, 4):      ", pow 3 4)
-  print ("tailPow(3, 4):  ", tailPow 3 4)
-  print ("---------------------------")
+  print ("------: LESSON 04 :----------")
+  print ("01: eGcd    (3, 27):     ", eGcd 3 27)
+  print ("02: pow      (3, 4):     ", pow 3 4)
+  print ("02: tailPow  (3, 4):     ", tailPow 3 4)
+  print ("04: perfect    (28):     ", perfect 28)
+  print ("-------------------------------")
